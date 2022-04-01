5063ac9aed02d3845fe2c734dd901f00dcdd352b
GJSayers <gemma.sayers1@btinternet.com>
2022-04-01 18:21:33 +0000

    Link css to Sidebar.js

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 // This componenet is a parent component
 function Sidebar() {
     return (
-      <div className="Sidebar">
+      <div className={css.sidebar}>
         <div>
-            <a>My Photos</a>
-            <a>My Illustrations</a>
-            <a>My Paintings</a>
+            <a> My Photos </a>
+
+            <a> My Illustrations </a>
+
+            <a> My Paintings </a>
         </div>
       </div>
     );
