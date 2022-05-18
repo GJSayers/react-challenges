7063d0cf201bb238e42eb726566bb1573affaf11
GJSayers <gemma.sayers1@btinternet.com>
2022-05-18 20:15:01 +0000

    Add content component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+import Sidebar from "./components/Sidebar";
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <Sidebar />
-
+      <Content />
+    
     </div>
   );
 }
