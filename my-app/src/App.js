e7aa2f716c064261605b9b466d7e6c3e61fa3f78
GJSayers <gemma.sayers1@btinternet.com>
2022-05-19 16:21:30 +0000

    Update app with search feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,31 @@ import Sidebar from "./components/Sidebar";
 import Content from './components/Content';
 import UncontrolledForm from './components/UncontrolledForm';
 import SearchBar from './components/SearchBar';
+import ControlledFormHooks from './components/ControlledFormHooks';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <div>
       <Sidebar />
+      </div>
+      
+      <div>
       <Content />
-      <UncontrolledForm />
-      <SearchBar />
+      </div>
+      
+      <div>
+      {/* <UncontrolledForm /> */}
+      </div>
+      
+      <div>
+      {/* <SearchBar /> */}
+      </div>
+      <div>
+      <ControlledFormHooks />
+      </div>
+      
     
     </div>
   );
